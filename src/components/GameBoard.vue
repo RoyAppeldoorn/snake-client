2242953413d0294b19c025fc9a2a656d25bfc7f2
Roy <roy.appeldoorn@gmail.com>
2019-12-03 13:55:02 +0100

    onMessageReceived func

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-btn @click="intervalStart">Send</v-btn>
+    <!-- <v-btn @click="move">Send</v-btn> -->
     <v-btn @click="disconnect">Stop</v-btn>
     <div id="log" style="font-size: 50px">{{ position }}</div>
     <canvas
@@ -28,35 +28,21 @@ export default {
   data() {
     return {
       connected: {},
-      position: {
-        x: 100,
-        y: 100
-      }
+      snakes: {}
     };
   },
   created() {
     this.socket = new SockJS("http://localhost:8090/snake");
     this.stompClient = Stomp.over(this.socket);
+    this.stompClient.debug = function() {};
   },
   mounted() {
     this.context = this.$refs.game.getContext("2d");
-    this.context.fillRect(this.position.x, this.position.y, 20, 20);
-
     this.stompClient.connect(
       {},
       () => {
         this.connected = true;
-        this.stompClient.subscribe(
-          "/topic/location",
-          message => {
-            console.log(message);
-            message.ack();
-            // for (var i = 0; i < data.length; i++) {
-            //   this.context.fillRect(data[i].x, data[i].y, 20, 20);
-            // }
-          },
-          { ack: "client" }
-        );
+        this.stompClient.subscribe("/topic/location", this.onMessageReceived);
       },
       error => {
         console.log(error);
@@ -68,6 +54,29 @@ export default {
     move() {
       if (this.stompClient && this.stompClient.connected) {
         this.stompClient.send("/app/update", JSON.stringify(this.position), {});
+      }
+    },
+    draw() {
+      this.context.clearRect(
+        0,
+        0,
+        this.$refs.game.width,
+        this.$refs.game.length
+      );
+    },
+    onMessageReceived(payload) {
+      var message = JSON.parse(payload.body);
+      if (message.type == "update") {
+        for (var i = 0; i < message.data.length; i++) {
+          // console.log(message.data[i].body[0].x);
+          // console.log(message.data[i].body[0].y);
+          this.context.fillRect(
+            message.data[i].body[0].x,
+            message.data[i].body[0].y,
+            20,
+            20
+          );
+        }
       }
     },
     disconnect() {
