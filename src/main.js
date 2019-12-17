bc5c508acc590c33e2effe9bbde7ec3d45de1d75
royappeldoorn <roy.appeldoorn@gmail.com>
2019-12-17 15:08:57 +0100

    added firebase config

--- src/main.js
+++ src/main.js
@@ -2,12 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import * as firebase from "firebase";
 
-import $ from "jquery";
 import vuetify from "./plugins/vuetify";
-window.jQuery = window.$ = $;
-require("bootstrap/dist/css/bootstrap.min.css");
-require("bootstrap/dist/js/bootstrap.min.js");
 
 Vue.config.productionTip = false;
 
@@ -17,3 +14,15 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount("#app");
+
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
