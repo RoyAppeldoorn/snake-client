2ec756d74c461e481d7f43cc53e495911b6454f3
royappeldoorn <roy.appeldoorn@gmail.com>
2020-01-07 22:50:24 +0100

    package update

---
+++ src/components/layout/NavigationDrawer.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-navigation-drawer>
+    <!-- -->
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Home", icon: "mdi-home-city" },
+        { title: "My Account", icon: "mdi-account" },
+        { title: "Users", icon: "mdi-account-group-outline" }
+      ]
+    };
+  }
+};
+</script>
