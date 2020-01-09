5c7865b1c64fcfc9b4d1586d04fc7d871239374f
royappeldoorn <roy.appeldoorn@gmail.com>
2020-01-09 13:52:15 +0100

    Activity log for displaying current connected users

---
+++ src/components/Activity.vue
@@ -0,0 +1,27 @@
+<template>
+  <table id="conversation" class="table table-striped">
+    <thead>
+      <tr>
+        <th>Players</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in activity.messages" :key="item">
+        <td>{{ item }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["activity"])
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
