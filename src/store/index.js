523db62ea8d06a1a57421edd5dc7b65dac62ad8a
royappeldoorn <roy.appeldoorn@gmail.com>
2020-01-09 13:53:26 +0100

    Added activity store to add messages to activity log

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import user from "@/store/modules/user.js";
-import websocket from "@/store/modules/websocket.js";
+import activity from "@/store/modules/activity.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: { user, websocket }
+  modules: { user, activity }
 });
